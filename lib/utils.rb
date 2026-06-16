f0d4ba0581616d328ebff7f32e196e08ba370c1b
Alex <140432662+a1xex@users.noreply.github.com>
2026-06-16 13:31:26 -0400

    Update utils.rb

--- lib/utils.rb
+++ lib/utils.rb
@@ -1,54 +1,37 @@
-# Class that will run all sync jobs
 class Util
-  # Method triggered from Discord
+
+  def self.role_group_map
+    map = {}
+    SiteSetting.discord_sync_role_group_map.split("|").each do |pair|
+      parts = pair.split(":", 2)
+      map[parts[0].strip] = parts[1].strip if parts.size == 2
+    end
+    map
+  end
+
   def self.sync_from_discord(discord_id)
-    # Search for users with the given Discord UD
     builder = DB.build("select u.* from user_associated_accounts uaa, users u /*where*/ limit 1")
     builder.where("provider_name = :provider_name", provider_name: "discord")
     builder.where("uaa.user_id = u.id")
-    builder.where("uaa.provider_uid = :discord_id", discord_id: discord_id)
+    builder.where("uaa.provider_uid = :discord_id", discord_id: discord_id.to_s)
 
     result = builder.query
 
-    if result.size == 0 then
-      
-      # No profile on Discourse? Then just remove all roles from Discord
-      Instance::bot.servers.each do |key, server|
-        member = server.member(discord_id)
-        member.roles.each do |role|
-          Instance::bot.send_message(SiteSetting.discord_bot_admin_channel_id, "@#{user.username} removed role #{role.name}")
-          member.remove_role(role)
-        end
-      end
-
+    if result.size == 0
+      Instance::bot.send_message(
+        SiteSetting.discord_sync_admin_channel_id,
+        "Discord user #{discord_id} has no linked Discourse account, skipping."
+      )
     else
-
-      # Process and sync the user using the standard Discourse method
       result.each do |t|
         self.sync_user(t)
       end
-
     end
   end
 
-  # Search for a role in the server with a given name
-  def self.find_role(role_name)
-    discord_role = nil
-    Instance::bot.servers.each do |key, server|
-      server.roles.each do |role|
-        if role.name == role_name then
-          discord_role = role
-        end
-      end
-    end
-    discord_role
-  end
-
-  # Sync users from Discourse to Discord
   def self.sync_user(user)
     discord_id = nil
 
-    # Fetch the Discord ID from database
     builder = DB.build("select uaa.provider_uid from user_associated_accounts uaa /*where*/ limit 1")
     builder.where("provider_name = :provider_name", provider_name: "discord")
     builder.where("uaa.user_id = :user_id", user_id: user.id)
@@ -56,57 +39,42 @@ class Util
       discord_id = t.provider_uid
     end
 
-    unless discord_id.nil? then
-      groups = []
+    return if discord_id.nil?
 
-      # Get user groups from database
-      builder = DB.build("select g.name from groups g, group_users gu /*where*/")
-      builder.where("g.visibility_level = :visibility", visibility: 0)
-      builder.where("g.id = gu.group_id")
-      builder.where("gu.user_id = :user_id", user_id: user.id)
-      builder.query.each do |t|
-        groups << t.name
-      end
-      
-      # For each server, just keep things synced
-      Instance::bot.servers.each do |key, server|
-        member = server.member(discord_id)
-        unless member.nil? then
+    mapping = self.role_group_map
+    return if mapping.empty?
 
-          # Make nickname the same as Discourse username
-          if member.nick != user.username && SiteSetting.discord_sync_username then
-            Instance::bot.send_message(SiteSetting.discord_sync_admin_channel_id, "Updated nickname @#{user.username}")
-            member.set_nick(user.username)
-          end
+    Instance::bot.servers.each do |key, server|
+      member = server.member(discord_id)
+      next if member.nil?
 
-          # If there is a verified role set, grant the user with that role
-          if SiteSetting.discord_sync_verified_role != "" then
-            role = self.find_role(SiteSetting.discord_sync_verified_role)
-            unless role.nil? || (member.role? role) then
-              Instance::bot.send_message(SiteSetting.discord_sync_admin_channel_id, "@#{user.username} granted role #{role.name}")
-              member.add_role(role)
-            end
-          end
+      member_role_names = member.roles.map(&:name)
 
-          # Remove all roles which are not safe, not the verified role or the user is not part of a group with that name
-          member.roles.each do |role|
-            unless (groups.include? role.name) || (SiteSetting.discord_sync_safe_roles.include? role.name) || role.name == SiteSetting.discord_sync_verified_role then
-              Instance::bot.send_message(SiteSetting.discord_sync_admin_channel_id, "@#{user.username} removed role #{role.name}")
-              member.remove_role(role)
-            end
-          end
+      mapping.each do |role_name, group_name|
+        group = Group.find_by(name: group_name)
+        next if group.nil?
 
-          # Add all roles which the user is part of a group
-          groups.each do |group|
-            role = self.find_role(group)
-            unless role.nil? || (member.role? role) then
-              Instance::bot.send_message(SiteSetting.discord_sync_admin_channel_id, "@#{user.username} granted role #{role.name}")
-              member.add_role(role)
-            end
-          end
+        discourse_user = User.find_by(id: user.id)
+        next if discourse_user.nil?
 
+        has_discord_role = member_role_names.include?(role_name)
+        in_discourse_group = GroupUser.exists?(group_id: group.id, user_id: discourse_user.id)
+
+        if has_discord_role && !in_discourse_group
+          group.add(discourse_user)
+          Instance::bot.send_message(
+            SiteSetting.discord_sync_admin_channel_id,
+            "Added @#{discourse_user.username} to Discourse group #{group_name} via Discord role #{role_name}"
+          )
+        elsif !has_discord_role && in_discourse_group
+          group.remove(discourse_user)
+          Instance::bot.send_message(
+            SiteSetting.discord_sync_admin_channel_id,
+            "Removed @#{discourse_user.username} from Discourse group #{group_name} via Discord role #{role_name}"
+          )
         end
       end
     end
   end
+
 end
